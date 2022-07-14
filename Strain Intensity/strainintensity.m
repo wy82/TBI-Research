fa1065cde9fc2d7c0d53e00d5d8c52972cfa526d
wy82 <wy82@cornell.edu>
2022-07-14 03:05:39 -0400

    Added GUI support

--- Strain Intensity/strainintensity.m
+++ Strain Intensity/strainintensity.m
@@ -1,23 +1,35 @@
-function strainintensity(Ns,Nc,Nd,name)
+function strainintensity(Ns,Nc,Nd,Nr,name)
     % Setup
+    %{
     a = arduino;
     configurePin(a,'D10','DigitalOutput');
     configurePin(a,'A0','AnalogInput');
+    %}
     si = zeros(Ns,Nc,Nd);
-    writeDigitalPin(a,'D10',true);
-    x = 0.2:0.2:20;
+    %writeDigitalPin(a,'D10',true);
+    x = linspace(0,Nr,Nd+1);
+    x = x(2:end);
+    sensors = 1:Ns;
     % LED
-    i = 1;
+    i = 0;
     j = 1;
     k = 1;
     period = 0.2;
-    while i <= Ns
+    while true
+        txt = input('Next Sensor: ','s');
+        if txt == 'q'
+            break;
+        end
+        if txt ~= 'r' || i == 0
+            i = str2double(txt);
+            sensors = sensors(sensors ~= i);
+        end
         disp(append('Sensor #',int2str(i)));
         while j <= Nc
             h = animatedline;
-            axis([0 20 0 5]);
+            axis([0 Nr 0 5]);
             input(append('Cycle #',int2str(j),': '));
-            pause(0.1)
+            pause(0.16)
             t0 = tic;
             while k <= Nd
                 delta = period*k - toc(t0);
@@ -27,25 +39,18 @@ function strainintensity(Ns,Nc,Nd,name)
                 end
                 disp(append('Displacement #',int2str(k),': '));
                 % Record intensity
-                si(i,j,k) = readVoltage(a,'A0');
+                %si(i,j,k) = readVoltage(a,'A0');
                 disp(si(i,j,k))
                 addpoints(h,x(k),si(i,j,k))
                 k = k + 1;
             end
             close all;
-
             k = 1;
             j = j + 1;
         end
-        i = i + 1;
         j = 1;
-        txt = input('','s');
-        if txt == 'r'
-            i = i - 1;
-            continue;
-        end
         save(name,'si');
     end
     save(name,'si');
-    clear a;
+    %clear a;
 end
--- Strain Intensity/strainintensity.py
+++ Strain Intensity/strainintensity.py
@@ -5,114 +5,222 @@ import pyautogui
 import time
 import re
 import PySimpleGUI as sg
-
-pyautogui.PAUSE = 0
+import numpy as np
 
 # Theme
-default = ('Helvetica', 12)
-sg.theme('TanBlue')
+default = ('Helvetica', 15)
+sg.theme('tanblue')
+bgcolor = sg.theme_background_color()
 
 # Layout
 column = [
     [
-        sg.Text("# of Sensors: "),
-        sg.In(size =(5,1), enable_events=True, key = "Ns")
+        sg.Text('# of Sensors: '),
     ],
     [
-        sg.Text("# of Cycles: "),
-        sg.In(size =(5,1), enable_events=True, key = "Nc")
+        sg.Text('# of Cycles: '),
     ],
     [
-        sg.Text("# of Displacements: "),
-        sg.In(size =(5,1), enable_events=True, key = "Nd")
+        sg.Text('# of Displacements: '),
     ],
     [
-        sg.Text("Test Run #: "),
-        sg.In(size =(5,1), enable_events=True, key = "Nt")
+        sg.Text('Test Run #: '),
     ],
     [
-        sg.Text("Save As: "),
-        sg.In(size =(5,1), enable_events=True, key = "name")
+        sg.Text('Max Displacement: '),
+    ],
+    [
+        sg.Text('Save As: '),
     ]
 ]
-
+inputcol = [
+    [
+        sg.In(size = (10,1), enable_events=True, key = 'Ns'),
+        sg.Text(size = (14, 1), font=('Helvetica', 9), justification='center', text_color = 'red', key='Nstext')
+    ],
+    [
+        sg.In(size = (10,1), enable_events=True, key = 'Nc'),
+        sg.Text(size = (14, 1), font=('Helvetica', 9), justification='center', text_color = 'red', key='Nctext')
+    ],
+    [
+        sg.In(size = (10,1), enable_events=True, key = 'Nd'),
+        sg.Text(size = (14, 1), font=('Helvetica', 9), justification='center', text_color = 'red', key='Ndtext')
+    ],
+    [
+        sg.In(size = (10,1), enable_events=True, key = 'Nt'),
+        sg.Text(size = (14, 1), font=('Helvetica', 9), justification='center', text_color = 'red', key='Nttext')
+    ],
+    [
+        sg.In(size = (10,1), enable_events=True, key = 'Nr'),
+        sg.Text(size = (14, 1), font=('Helvetica', 9), justification='center', text_color = 'red', key='Nrtext')
+    ],
+    [
+        sg.In(size =(10,1), enable_events=True, key = 'name')
+    ]
+]
+button = sg.Button('Begin Test', bind_return_key = True)
+buttoncol = [
+    [button]
+]
 layout = [
-    [sg.Column(column, vertical_alignment = "left")],
-    [sg.Button("Begin Test")]
+    [sg.Column(column, justification = 'center'), sg.Column(inputcol)],
+    [sg.Column(buttoncol, justification = 'center')]
 ]
 
+
 # Window
+window = sg.Window('Strain Intensity Test', layout, grab_anywhere = True, size = (350,300), font = default)
 
-window = sg.Window('Strain Intensity Test', layout, grab_anywhere = True, size = (350,300), font = default, resizable = True)
-
+new = True
 # Event Loop
 while True:
+    if new:
+        pyautogui.getWindowsWithTitle('Strain Intensity Test')[0].activate()
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Exit':
+    if event == sg.WIN_CLOSED:
         break
-    if event == "Begin Test":
-        Ns = int(values["Ns"])
-        Nc = int(values["Nc"])
-        Nd = int(values["Nd"])
-        Nt = int(values["Nt"])
-        name = values["name"]
-        measurements = 0
-        cycles = Nc*Ns
-        nums = []
-        # Start up MATLAB script
-        pyautogui.getWindowsWithTitle("MATLAB R2022a - academic use")[0].activate()
-        pyautogui.write("strainintensity({}, {}, {}, '{}')".format(Ns, Nc, Nd, name))
-        pyautogui.press("enter")
-
-        while cycles > 0:
-            time.sleep(3)
-            # Start up UniVert
-            titles = pyautogui.getAllTitles()
-            r = re.compile("UniVert.*")
-            uni = list(filter(r.match,titles))
-            pyautogui.getWindowsWithTitle(uni[0])[0].activate()
-            # Create new test
-            pyautogui.leftClick(x = 60, y = 96)
-            time.sleep(2)
-            pyautogui.hotkey('ctrl','n')
-            pyautogui.press('enter')
-            time.sleep(8)
-            # Reset displacement
-            pyautogui.doubleClick(x = 186, y = 81)
-            time.sleep(8)
-            # Wait for input to stat
-            if cycles % Nc == 0:
-                input("Sensor #" + str((Nc*Ns - cycles)//Nc + 1))
-                pyautogui.getWindowsWithTitle("Command Prompt")[0].minimize()
-                titles = pyautogui.getAllTitles()
-                r = re.compile("UniVert.*")
-                uni = list(filter(r.match,titles))
-                pyautogui.getWindowsWithTitle(uni[0])[0].activate()
-            time.sleep(1)
-            cycles = cycles - 1
-            measurements = Nd
-            # Start test
-            pyautogui.leftClick(x = 29, y = 96)
-            pyautogui.getWindowsWithTitle("MATLAB R2022a - academic use")[0].activate()
-            time.sleep(0.1)
-            pyautogui.press('enter')
-            time.sleep(21)
-
-            if cycles % Nc == 0:
-                txt = input("Press 'r' to redo: ")
-                # Continue
-                if txt != 'r':
-                    pyautogui.getWindowsWithTitle("MATLAB R2022a - academic use")[0].activate()
-                    pyautogui.press('enter')
-                    nums.append(Nt-Nc+1)
-                # Redo
-                else: 
-                    pyautogui.getWindowsWithTitle("MATLAB R2022a - academic use")[0].activate()
-                    pyautogui.write('r')
-                    pyautogui.press('enter')
-                    cycles = cycles + Nc
-            Nt = Nt + 1
-        # Print test run numbers
-        print(nums)
+    validInput = True
+    if event == 'Begin Test':
+        digits = re.compile('\d+')
+        keys = ['Ns','Nc','Nd','Nt','Nr']
+        for k in keys:
+          if digits.match(values[k]) is None:
+            validInput = False
+            window[k].Widget.configure(highlightcolor = 'red', highlightbackground ='red', highlightthickness=1)
+            window[k+'text'].update('Must be a #')
+        if validInput:
+            for k in keys:
+                window[k].Widget.configure(highlightcolor = bgcolor, highlightbackground = bgcolor)
+                window[k+'text'].update('')
+            Ns = int(values['Ns'])
+            Nc = int(values['Nc'])
+            Nd = int(values['Nd'])
+            Nt = int(values['Nt'])
+            Nr = int(values['Nr'])
+            name = values['name']
+            cycles = 0
+            sensors = np.arange(1,Ns+1)
+            prev = sensors[0]
+            nums = []
+            break
 window.close()
 
+# Start up MATLAB script  
+pyautogui.getWindowsWithTitle('MATLAB R2022a - academic use')[0].activate()
+pyautogui.write("strainintensity({}, {}, {}, {}, '{}')".format(Ns, Nc, Nd, Nr, name))
+pyautogui.press('enter')
+
+while True:
+    time.sleep(3)
+    # Start up UniVert
+    titles = pyautogui.getAllTitles()
+    r = re.compile('UniVert.*')
+    uni = list(filter(r.match,titles))
+    pyautogui.getWindowsWithTitle(uni[0])[0].activate()
+    # Stop current test
+    pyautogui.leftClick(x = 60, y = 96)
+    time.sleep(2)
+    # Creat new test
+    pyautogui.hotkey('ctrl','n')
+    pyautogui.press('enter')
+    time.sleep(8)
+    # Reset displacement
+    pyautogui.leftClick(x = 186, y = 81)
+    time.sleep(8)
+    # Wait for input to start
+
+    if cycles == 0:
+        # Layout
+        cycles = Nc
+        if sensors.size > 0:
+            sensornum = sensors[0]
+        else:
+            sensornum = prev
+        text = 'Sensor #' + str(sensornum)
+        button = sg.Button('Begin Test')
+        redob = sg.Button('Redo')
+        exitb = sg.Button('Finish')
+        col = [
+            [sg.Text('New Sensor #: ', size = (12,1), font = ('Helvetica', 15))]
+        ]
+        inputcol = [
+            [
+                sg.In(size = (10,1), key = 'newsensor', do_not_clear = False)
+            ],
+            [
+                sg.Text(size = (14,1), font = ('Helvetica', 9), text_color = 'red', key='newsensortext')
+            ]
+        ]
+        buttoncol = [
+            [button,redob,exitb]
+        ]
+        layout = [
+            [sg.Column(col, vertical_alignment = 'top',justification = 'center',key = 'col'),sg.Column(inputcol,key = 'inputcol')],
+            [sg.Column(buttoncol, justification = 'center')]
+        ]
+        # Window  
+        window = sg.Window('Input', layout, grab_anywhere = True, size = (250,150), font = default, finalize = True)
+        window['newsensor'].bind('<Return>', '_enter')
+        if sensornum == 1 or sensors.size <= 0:
+            window['Redo'].update(visible = False)
+        if sensors.size > 0:
+            window['Finish'].update(visible = False)
+        button.update(text)
+        redob.update('Redo #' + str(prev))
+        command = ''
+        new = True
+        # Event Loop
+        while True:
+            if new:
+                new = False
+                pyautogui.getWindowsWithTitle('Input')[0].activate()
+            event, values = window.read()
+            if event == sg.WIN_CLOSED:
+                break
+            validInput = True
+            if event == 'Begin Test':
+                prev = sensornum
+                command = str(sensornum)
+                sensors = sensors[sensors != sensornum]
+                break
+            if event == 'newsensor_enter':
+                digits = re.compile('\d+')
+                if digits.match(values['newsensor']) is None or int(values['newsensor']) > Ns or sensornum == 1 and int(values['newsensor']) == 1:
+                    validInput = False
+                    window['newsensor'].Widget.configure(highlightcolor = 'red', highlightbackground ='red', highlightthickness=1)
+                    window['newsensortext'].update('Must be a # <' + str(Ns))
+                if validInput:
+                    button.update('Sensor #' + values['newsensor'])
+                    sensornum = int(values['newsensor'])
+                    window['newsensor'].Widget.configure(highlightcolor = bgcolor, highlightbackground = bgcolor)
+                    window['newsensortext'].update('')
+            if event == 'Redo':
+                sensornum = prev
+                command = 'r'
+                break
+            if event == 'Finish':
+                command = 'q'
+                break
+        window.close()
+        pyautogui.getWindowsWithTitle('MATLAB R2022a - academic use')[0].activate()
+        pyautogui.write(command)
+        pyautogui.press('enter')
+        if command == 'q':
+            break
+        titles = pyautogui.getAllTitles()
+        r = re.compile('UniVert.*')
+        uni = list(filter(r.match,titles))
+        pyautogui.getWindowsWithTitle(uni[0])[0].activate()
+    
+    time.sleep(1)
+    cycles = cycles - 1
+    # Start test  
+    pyautogui.leftClick(x = 29, y = 96)
+    pyautogui.getWindowsWithTitle('MATLAB R2022a - academic use')[0].activate()
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(21)
+    
+    Nt = Nt + 1
+# Print test run numbers
+print(nums)
